70dd45a7ac03f13c92cf0ea76df125b44d2020d0
112kutiko <tadask04@gmail.com>
2021-02-04 17:28:47 +0200

    Update meniu_hl.cs

--- Assets/scripts/meniu_hl.cs
+++ Assets/scripts/meniu_hl.cs
@@ -24,8 +24,11 @@ public class meniu_hl : MonoBehaviour
         {
         url_link = Username_field.text.ToString();
         PlayerPrefs.SetString("url_1", url_link);
-            _c_lat = float.Parse(_lat.text);
-            _c_long = float.Parse(_long.text);
+            string t1 = _lat.text.ToString();
+            string t2 = _long.text.ToString();
+            _c_lat =  float.Parse(t1);
+            _c_long = float.Parse(t2);
+
             PlayerPrefs.SetFloat("_lat_c",_c_lat);
             PlayerPrefs.SetFloat("_long_c", _c_long);
         }
