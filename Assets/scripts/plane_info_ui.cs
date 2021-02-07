3d24f546c724fc1c0d7b14806e84a33d7c69dffc
112kutiko <tadask04@gmail.com>
2021-02-07 14:37:32 +0200

    Create plane_info_ui.cs

---
+++ Assets/scripts/plane_info_ui.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plane_info_ui : MonoBehaviour
+{
+    public static radar instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
