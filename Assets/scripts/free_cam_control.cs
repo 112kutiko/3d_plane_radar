80af84631f52769b0865d4ea24355ffa0c479ab5
112kutiko <tadask04@gmail.com>
2021-02-10 10:21:28 +0200

    some update

--- Assets/scripts/free_cam_control.cs
+++ Assets/scripts/free_cam_control.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class free_cam_control : MonoBehaviour
 {
+    public float speed = 6.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    public float mouseSensitivity = 100f;
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (radar.instance._now_plane == "" || radar.instance._now_plane == string.Empty)
+        {
+            float h = 2 * Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            transform.Rotate(0, h, 0);
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+
     }
 }
---
+++ Assets/scripts/free_cam_view.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class free_cam_view : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    float xr_1 = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(radar.instance._now_plane==""|| radar.instance._now_plane == string.Empty)
+        {
+            float y = 2 * Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            xr_1 -= y;
+            xr_1 = Mathf.Clamp(xr_1, -90f, 63f);
+            transform.localRotation = Quaternion.Euler(xr_1, 0f, 0f);
+        }
+
+    }
+
+    
+
+}
