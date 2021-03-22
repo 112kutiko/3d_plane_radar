a91a98233236c1285deb0f2e1d4b6a3e216c866d
112kutiko <tadask04@gmail.com>
2021-03-22 22:59:40 +0200

    fdebug.log

--- Assets/scripts/debug_ss.cs
+++ Assets/scripts/debug_ss.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class debug_ss : MonoBehaviour
 {
     public string output = "";
     public string stack = "";
+    string hh,tmp="";
+    public Text con;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if(tmp!= output)
+        {
+        hh +=output + "\n";
+        con.text = hh;  
+        tmp = output;
+        }
+        else
+        {
+            tmp = output;
+        }
+
     }
 
 
