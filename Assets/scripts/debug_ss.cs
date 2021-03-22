5294979577392adba1ce1894e376877aa83e896d
112kutiko <tadask04@gmail.com>
2021-03-22 12:27:04 +0200

    update

---
+++ Assets/scripts/debug_ss.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debug_ss : MonoBehaviour
+{
+    public string output = "";
+    public string stack = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        output = logString;
+        stack = stackTrace;
+    }
+}
