40f3a40eac945fc8f304e82f7e4253d8d5e441a8
112kutiko <tadask04@gmail.com>
2023-01-06 23:07:38 +0200

    update meniu script
    atnaujintas meniu scriptas

--- Assets/scripts/meniu_hl.cs
+++ Assets/scripts/meniu_hl.cs
@@ -5,93 +5,63 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Globalization;
 
-
-public class meniu_hl : MonoBehaviour
-{
+public class meniu_hl : MonoBehaviour{
     public Text Username_field, _long, _lat;
     public string url_link;
     public float _c_long=0f, _c_lat = 0f;
     public GameObject m_ui, s_ui;
-    private int cop = 0;
 
-    void Update()
-    {
-      
-        if (cop == 0)
-        {
-            url_link = Username_field.text.ToString();
-            PlayerPrefs.SetString("url_1", url_link);  
-            float tmp_c_m;
-                    if (float.TryParse(_lat.text, out tmp_c_m))
-                    {
-                        Debug.Log("lat: "+tmp_c_m);
-                        _c_lat = tmp_c_m;
-                    }
-                    else
-                    {
-                        Debug.Log("lat fail: "+ _lat.text);
-                    }
-                      if (float.TryParse(_long.text, out _c_long))
-                    {
-                        Debug.Log("long: " + _c_long);
-                    }
-                    else
-                     {
-                        Debug.Log("long fail: " + _long.text);
+ void Update() {
+  ParseLatitude(_lat.text);
+  ParseLongitude(_long.text);
+                }
 
-                     }
-             PlayerPrefs.SetFloat("_lat_c",_c_lat);
-             PlayerPrefs.SetFloat("_long_c", _c_long); 
-        }
-        
-
-    }
-
-    public void map_on()
-    {
+    public void map_on() {
          if (url_link.Contains("AircraftList.json"))
         {
             if(_c_lat!=0f && _c_long != 0f)
             {
              Debug.Log("1");
              SceneManager.LoadScene(1);
-             cop = 1;
-            }
-            else
-            {
+    }else{
                 Debug.Log("fail");
             }
-           
-        }
-        else
-        {
+         } else {
             if (_c_lat != 0f && _c_long != 0f)
             {
-                cop = 1;
                 Debug.Log("2");
                 url_link = "http://127.0.0.1/VirtualRadar/AircraftList.json";
                 PlayerPrefs.SetString("url_1", url_link);
                 SceneManager.LoadScene(1);
-            }
-            else
-            {
+            }  else {
                 Debug.Log("fail");
             }
         }
     }
-    public void change_ui()
-    {
-        if (m_ui.activeSelf)
+    public void ParseLatitude(string lat)  {
+        float tmp_c_m;
+        if (float.TryParse(lat, out tmp_c_m))
         {
-            m_ui.SetActive(false);
-            s_ui.SetActive(true);
+            Debug.Log("lat: " + tmp_c_m);
+            _c_lat = tmp_c_m;
+
+            PlayerPrefs.SetFloat("_lat_c", _c_lat);
+        
+        }else {
+            Debug.Log("lat fail: " + lat);
         }
-        else
-        {
-            m_ui.SetActive(true);
-            s_ui.SetActive(false);
-        }
+
     }
 
-
+    public void ParseLongitude(string lon)  {
+        float tmp_c_m;
+        if (float.TryParse(lon, out float tmp_c_m))
+        {
+            _c_long=tmp_c_m;
+            Debug.Log("long: " + _c_long);
+            PlayerPrefs.SetFloat("_long_c", _c_long);
+        }else{
+            Debug.Log("long fail: " + lon);
+        }
+    }
 }
